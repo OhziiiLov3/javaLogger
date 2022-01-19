84f58b8b8817d3a7ea430eebe56c2c8627648234
ohziiilov3 <klbaskerville0520@gmail.com>
2022-01-19 10:48:52 -0800

    adding Garbage Collection Notes

---
+++ src/javaLogger/garbageCollection.java
@@ -0,0 +1,21 @@
+package javaLogger;
+
+ class Dog {
+	
+	private String name;
+	
+	public Dog (String name) {
+		this.name = name;
+	}
+	public String toString() {
+		return name;
+	}
+
+}
+
+public class garbageCollection {
+	
+	public static void main(String[] args) {
+		
+	}
+}
