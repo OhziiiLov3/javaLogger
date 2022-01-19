4b4e5a6c2b18cb299b9df8724a9bc82f1e5692a0
ohziiilov3 <klbaskerville0520@gmail.com>
2022-01-19 11:02:16 -0800

    Garbage Collection Notes Updated

--- src/javaLogger/garbageCollection.java
+++ src/javaLogger/garbageCollection.java
@@ -1,6 +1,7 @@
 package javaLogger;
-
- class Dog {
+//Garbage collection is an automatic memory management system by Java, it allocates memory to the heap
+ 
+class Dog {
 	
 	private String name;
 	
@@ -15,7 +16,16 @@ package javaLogger;
 
 public class garbageCollection {
 	
+	private static Dog d4;
 	public static void main(String[] args) {
+		Dog d1 = new Dog("Zooty");
+		Dog d2 = new Dog("Cali");
+		Dog d3 = new Dog("Zoey");
+		Dog d4 = new Dog("Koko");
+		
+		d3 = new Dog("Fefe");
+//		Cali is pushed to the garbage collector as we add a new object called
+		System.out.println(d3);
 		
 	}
 }
